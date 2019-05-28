bab37897746067778b2d383ec45cfb7812929a9c
Tyler Hueter <tylerhueter08@gmail.com>
2019-05-28 15:23:41 -0400

    sign up

--- policomp/app/controllers/api/sessions_controller.rb
+++ policomp/app/controllers/api/sessions_controller.rb
@@ -3,12 +3,12 @@ class Api::SessionsController < ApplicationController
     def create 
 
         @user = User.find_by(email: params[:email]) 
-       
+        
         if @user && @user.authenticate(params[:password])
-            token = JWT.encode({ user_id: @user.id }, ENV['SECRETS'], 'HS256')
+            token = JWT.encode({ user_id: @user.id }, ENV['AUTH_JWT'], 'HS256')
             render json: { token: token}, status: :ok
         else
-            render json: {errors: ['Incorrect email or password']},
+            render json: {errors: ['Incorrect email and password']},
                     status: :unathorized
         end
     end 
--- policomp/config/routes.rb
+++ policomp/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :sources, only: [:index, :create]
     resources :articles, only: [:index, :create]
-    resources :user, only: [:index, :create]
+    resources :users, only: [:index, :create]
     resources :user_stories, only: [:index, :create, :update]  
     post '/login' , to: 'sessions#create', as: 'login'
   end
